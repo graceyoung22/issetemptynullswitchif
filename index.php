6322db0212bf0d0cef60723ee9c5d5285a366ae5
Grace A Young <gay5@afsconnect1.njit.edu>
2016-10-30 17:32:14 -0400

    "4 switches

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+//4 ifs
 if ($weather == rain) {
 	echo "bring an umbrella";
 } elseif ($weather == sun) {
@@ -35,5 +36,54 @@ if (isset($var3)) {
 } else {
 	echo "False";
 }	
+
+// Switch-cases
+switch ($weather1) {
+	case "rain":
+		echo "bring an umbrella";
+		break;
+	case "sun":
+		echo "enjoy your day";
+		break;
+	case "cloudy":
+		echo "eh it's 50/50";
+		break;
+}		
+
+switch (isset($var4)) {
+	case "":
+		echo "True";
+		break;
+	case " ":
+		echo "True";
+		break;
+	case NULL:
+		echo "False";
+		break;
+}		
+
+switch (empty($var5)) {
+	case "":
+		echo "True";
+		break;
+	case " ":
+		echo "False";
+		break;
+	case NULL:
+		echo "True";
+		break;
+}
+
+switch (is_null($var6)) {
+	case "":
+		echo "False";
+		break;
+	case " ":
+		echo "False";
+		break;
+	case NULL:
+		echo "True";
+		break;
+}
 
 ?>
